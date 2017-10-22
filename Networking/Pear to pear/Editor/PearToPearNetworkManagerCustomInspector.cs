509695b1a217fbc00b5ad220338774dcd2d67be1
ukidabek <ukidabek@gmail.com>
2017-10-22 16:13:17 +0200

    - Network update.

---
+++ Networking/Pear to pear/Editor/PearToPearNetworkManagerCustomInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BaseGameLogic.Networking.PearToPear
+{
+    [CustomEditor(typeof(PearToPearNetworkManager), true)]
+    public class PearToPearNetworkManagerCustomInspector : Editor
+    {
+        private PearToPearNetworkManager manager = null;
+
+        private void OnEnable()
+        {
+            manager = target as PearToPearNetworkManager;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if(GUILayout.Button("Connect"))
+            {
+                manager.Connect();
+            }
+        }
+    }
+}
--- Networking/Pear to pear/PearToPearNetworkManager.cs
+++ Networking/Pear to pear/PearToPearNetworkManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ namespace BaseGameLogic.Networking.PearToPear
     public abstract class PearToPearNetworkManager : MonoBehaviour
     {
         [SerializeField]
-        protected int port = 8888;
+        private int port = 8888;
 
         [SerializeField]
         protected int connectionsCount = 8;
@@ -35,14 +36,53 @@ namespace BaseGameLogic.Networking.PearToPear
             // Topology configuration
             HostTopology topology = new HostTopology(config, connectionsCount);
 
-            hostID = NetworkTransport.AddHost(topology, port);
-
+            int portToUse = _pearType == PearToPearNetworkManagerEnum.MasterPear ? port : 0;
+            hostID = NetworkTransport.AddHost(topology, portToUse);
         }
 
         protected void AddChanel(ref ConnectionConfig conectionConfig, QosType type)
         {
             int channelId = conectionConfig.AddChannel(type);
             channelDictionary.Add(type, channelId);
+        }
+
+        protected virtual void Update()
+        {
+            if (_pearType == PearToPearNetworkManagerEnum.Pear)
+                return;
+
+            int recHostId;
+            int connectionId;
+            int channelId;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            byte error;
+            NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+            switch (recData)
+            {
+                case NetworkEventType.Nothing:         //1
+                    break;
+                case NetworkEventType.ConnectEvent:    //2
+                    int port;
+                    string adres = string.Empty;
+                    NetworkID networkID;
+                    NodeID node;
+                    NetworkTransport.GetConnectionInfo(hostID, connectionId, out adres, out port, out networkID, out node, out error);
+                    Debug.Log(port);
+                    break;
+                case NetworkEventType.DataEvent:       //3
+                    break;
+                case NetworkEventType.DisconnectEvent: //4
+                    break;
+            }
+        }
+
+        public void Connect()
+        {
+            int connectionId;
+            byte error;
+            connectionId = NetworkTransport.Connect(hostID, "127.0.0.1", port, 0, out error);
         }
     }
 }

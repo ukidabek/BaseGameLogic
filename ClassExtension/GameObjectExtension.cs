3fd9c3f807190e7311b65128604c01205d70e3c1
ukidabek <ukidabek@gmail.com>
2018-02-06 09:38:59 +0100

    - Game object static functions added.

--- ClassExtension/GameObjectExtension.cs
+++ ClassExtension/GameObjectExtension.cs
@@ -17,4 +17,39 @@ public static class GameObjectExtension
         component = gameObject.GetComponentInParent<T>();
         return component;
     }
+
+    public static T CreateInstance<T>(GameObject prefab, Transform parrent = null, string objectName = "") where T : Component
+    {
+        GameObject newObject = GameObject.Instantiate(prefab);
+        newObject.name = !string.IsNullOrEmpty(objectName) ? objectName : newObject.name;
+        T newComponent = newObject.GetComponent<T>();
+
+        newObject.transform.SetParent(parrent);
+
+        newObject.transform.localPosition = Vector3.zero;
+        newObject.transform.localRotation = Quaternion.identity;
+        newObject.transform.localScale = Vector3.one;
+
+        return newComponent;
+    }
+
+    public static T CreateObjectWithComponent<T>(Transform parrent = null, string objectName = "") where T : Component
+    {
+        GameObject newGameObject = new GameObject();
+
+        if (!string.IsNullOrEmpty(objectName))
+        {
+            newGameObject.name = objectName;
+        }
+
+        newGameObject.transform.SetParent(parrent);
+
+        newGameObject.transform.localPosition = Vector3.zero;
+        newGameObject.transform.localRotation = Quaternion.identity;
+        newGameObject.transform.localScale = Vector3.one;
+
+        T component = newGameObject.AddComponent<T>();
+
+        return component;
+    }
 }
